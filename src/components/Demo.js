67439c5456ede618bd46046b58f1e4ba3de4aa40
VijayKrishna549 <vijayk@bipp.io>
2023-02-15 12:13:32 +0530

    initial commit on ToDo App

---
+++ src/components/Demo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Demo.css";
+
+function Demo() {
+  return (
+    <div className="demoP">
+      <div className="one">
+        <h1 className="todoTitle">ToDo List</h1>
+      </div>
+
+      <div className="two">
+        <h1 className="todoTitle">ToDo List Two</h1>
+      </div>
+
+      <div className="three">
+        <h1 className="todoTitle">ToDo List Three</h1>
+      </div>
+
+      <div className="four">
+        <h1 className="todoTitle">ToDo List Three</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Demo;
